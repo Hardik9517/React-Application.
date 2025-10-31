a2d2f2ac625f5fedbeae6057a3866a0e46e9cb03
HARDIK <mahajanhardik20@gmail.com>
2025-10-31 22:33:24 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 React Docker Multi-Stage Build Example</h1>
+      <p>Containerized React app running successfully!</p>
+    </div>
+  );
+}
+
+export default App;
